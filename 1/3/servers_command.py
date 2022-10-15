0dc549440af113f5fba366f4f289dad89fcbef5c
Shubhendu Shekhar <63819260+shubhshekhar09@users.noreply.github.com>
2022-10-15 10:54:55 +0530

    Create servers_command.py

---
+++ 1/3/servers_command.py
@@ -0,0 +1,35 @@
+import cmd
+from http import client
+from sys import stderr, stdin, stdout
+import paramiko
+host_file = open('hostfile','rU')
+hostnames = host_file.readlines()
+
+port = 22
+
+try:
+    while True:
+        try:
+            cmd = input(">")
+            if cmd == 'exit': break
+            for node in hostnames:
+                client = paramiko.SSHClient()
+                client.load_system_host_keys()
+                client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+                user = input('Enter Username for the ' + node + ':')
+                passwd = input('Enter the Password for the ' + node + ':')
+                client.connect(node.strip(), port=port, username=user, password=passwd )
+                stdin, stdout, stderr = client.exec_command(cmd)
+                exit_status = stdout.channel.recv_exit_status() 
+                if exit_status == 0:
+                    print("Command has executed on host" + node + ": " + cmd)
+                    print(stdout.read().decode())
+                    print("Done")
+                else:
+                    print("Error",exit_status)
+                    print(stderr.read().decode())
+                client.close() 
+        except KeyboardInterrupt:
+            break   
+except Exception as err:
+    print(str(err))
